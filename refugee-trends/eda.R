25e3d41ed3839f9005e0202e63ad95cb51ffc4f0
Connor Rothschild <connorrothschild@gmail.com>
2020-02-01 16:37:23 -0600

    refugee code

--- refugee-trends/eda.R
+++ refugee-trends/eda.R
@@ -1,11 +1,12 @@
 library(tidyverse)
 
-data <- readr::read_csv("./data/final/data.csv")
+data <- readr::read_csv("./data/final/final_data.csv")
 
 table(data$region_name_3)
 
 grouped_data <- data %>% 
   mutate(region_name_3 = parse_number(region_name_3)) %>% 
+  filter(region_name_3 != 4) %>% 
   group_by(nat_definition4, region_name_3) %>% 
   slice(1) %>% 
   filter(!is.na(region_name_3))
@@ -17,4 +18,4 @@ grouped_data %>%
   geom_line()
 
 ## output into the non-R folder
-write.csv(grouped_data, "../data/data.csv")
+write.csv(grouped_data, "./data/final/final_data.csv")
--- refugee-trends/plot.R
+++ refugee-trends/plot.R
@@ -4,18 +4,49 @@ library(cr)
 
 set_cr_theme()
 
-data <- readr::read_csv("../data/data.csv")
+data <- readr::read_csv("./data/final/final_data.csv")
 
 data <- data %>% 
   mutate(state = openintro::state2abbr(nat_definition4)) %>% 
   mutate(region_name_3 = as.numeric(region_name_3))
+
+options(scipen = 999)
 
 data %>% 
   group_by(region_name_3) %>% 
   summarise(textbox37 = sum(textbox37)) %>% 
   ggplot(aes(x = region_name_3, y = textbox37)) +
   geom_line() +
-  geom_rect(mapping=aes(xmin=2017, xmax=2019, ymin=0, ymax=12000))
+  labs(title = "Refugee Acceptance on the Decline",
+       subtitle = "Number of refugees accepted annually, 2002 - 2019",
+       x = element_blank(),
+       y = element_blank()) +
+  scale_y_continuous(labels = scales::comma_format()) +
+  scale_x_continuous(breaks = c(2002, 2019))
+
+data %>% 
+  group_by(region_name_3, state) %>% 
+  summarise(textbox37 = sum(textbox37)) %>% 
+  ggplot(aes(x = region_name_3, y = textbox37)) +
+  geom_line(color = "black") +
+  geom_rect(mapping=aes(xmin=2009, xmax=2017, ymin=0, ymax=12000), fill = "#ADD8E6", alpha = .05) +
+  geom_rect(mapping=aes(xmin=2017, xmax=2019, ymin=0, ymax=12000), fill = "#FF9999", alpha = .05) +
+  scale_x_continuous(breaks = c(2002,2019)) +
+  scale_y_continuous(breaks = c(0,12000)) +
+  facet_geo(~ state, grid = "us_state_grid1") +
+  labs(title = "Refugee Acceptance on the Decline",
+       subtitle = "Number of refugees accepted annually, 2002 - 2019",
+       x = element_blank(),
+       y = element_blank()) +
+  theme(
+    axis.text.x = element_blank(),
+    axis.ticks.x = element_blank(),
+    axis.line.x = element_blank(),
+    axis.line.y = element_blank(),
+    plot.title = element_text(size = 24),
+    plot.subtitle = element_text(size = 18),
+    strip.background = element_rect(color = "white")
+  )
 
 data %>% 
   group_by(region_name_3, state) %>% 
